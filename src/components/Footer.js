2ecd3c638052a0178e8c4b1ecc6dfa471e1223f6
Natroth <nathanrth15@gmail.com>
2020-09-16 13:11:23 -0400

    footer and near complete

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,12 +10,10 @@ function Footer() {
         <footer className="mt-5 bg-white">
             <Container fluid={true}>
                 <Row className="border-top justify-content-between p-3 footer">
-                    <Col className="p-0 w-auto" md={3} sm={12}>
-                    Built with React
-                    </Col>
+
                     <Col className="d-flex justify-content-around bottom-links">
-                        <a href="https://www.linkedin.com/in/nathan-roth-260b31187/" target="_blank"><FaLinkedin /></a>
-                        <a href="https://github.com/Natroth" target="_blank"><FaGithub /></a>
+                        <a href="https://www.linkedin.com/in/nathan-roth-260b31187/" target="_blank"><FaLinkedin className="inner-link" /></a>
+                        <a href="https://github.com/Natroth" target="_blank"><FaGithub className="inner-link" /></a>
                     </Col>
                     <Col className="p-0 d-flex justify-content-end w-auto" md={3} sm={12}>
                     Nathan Roth
